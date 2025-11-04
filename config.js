0a8a3ba1fa56e182f32d0f5abe9683cfd0eff5de
Herbatniczek <115565033+herbatniczek0@users.noreply.github.com>
2025-11-04 18:04:48 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,393 @@
+// KONFIG — wybierz TŁO (YouTube *link* albo lokalny MP4) i MUZYKĘ.
+window.BIO_CONFIG = {
+    displayName: "Matsoor",
+    description: "19 y/o loser | just a dumb teenager!",
+
+    discord: {
+        userId: "1161001314856210583",
+        tag: "matsoor_",
+        status: "dnd",
+        addLink: "https://discord.com/users/1161001314856210583",
+        guildInvite: "https://discord.gg/akmfzEpzM8",
+
+        // Najprościej: podaj gotowy URL avatara i masz spokój.
+        avatarUrl: "https://cdn.discordapp.com/avatars/1161001314856210583/bdb4d907879e1657a39012a112212b9b.png?size=256",
+
+        userSub: "Chcesz własną strone/bota? Napisz na PV!"
+    },
+
+    // Linki społecznościowe (naprawiona ikona IG + website)
+    socials: [{
+        label: "",
+        url: "https://www.youtube.com/@m4ts00r",
+        iconSvg: '<svg role="img" viewBox="0 0 24 24"><path fill="currentColor" d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"></path></svg>'
+    }, {
+        label: "",
+        url: "https://www.twitch.tv/matsoor",
+        iconSvg: '<svg role="img" viewBox="0 0 24 24"><path fill="currentColor" d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z"></path></svg>'
+    }, {
+        label: "",
+        url: "https://www.tiktok.com/@matsoor",
+        iconSvg: '<svg role="img" viewBox="0 0 24 24"><path fill="currentColor" d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z"></path></svg>'
+    }, {
+        label: "",
+        url: "https://www.instagram.com/m4ts00r/",
+        iconSvg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M7 2h10c2.8 0 5 2.2 5 5v10c0 2.8-2.2 5-5 5H7c-2.8 0-5-2.2-5-5V7c0-2.8 2.2-5 5-5zm5 3.6a6.4 6.4 0 1 0 0 12.8 6.4 6.4 0 0 0 0-12.8zm0 2.2a4.2 4.2 0 1 1 0 8.4 4.2 4.2 0 0 1 0-8.4zM19 5.8a1.3 1.3 0 1 0 0 2.6 1.3 1.3 0 0 0 0-2.6z"/></svg>'
+    }, {
+        label: "",
+        url: "https://steamcommunity.com/id/mateku_/",
+        iconSvg: '<svg role="img" viewBox="0 0 24 24"><path fill="currentColor" d="M11.979 0C5.678 0 .511 4.86.022 11.037l6.432 2.658c.545-.371 1.203-.59 1.912-.59.063 0 .125.004.188.006l2.861-4.142V8.91c0-2.495 2.028-4.524 4.524-4.524 2.494 0 4.524 2.031 4.524 4.527s-2.03 4.525-4.524 4.525h-.105l-4.076 2.911c0 .052.004.105.004.159 0 1.875-1.515 3.396-3.39 3.396-1.635 0-3.016-1.173-3.331-2.727L.436 15.27C1.862 20.307 6.486 24 11.979 24c6.627 0 11.999-5.373 11.999-12S18.605 0 11.979 0zM7.54 18.21l-1.473-.61c.262.543.714.999 1.314 1.25 1.297.539 2.793-.076 3.332-1.375.263-.63.264-1.319.005-1.949s-.75-1.121-1.377-1.383c-.624-.26-1.29-.249-1.878-.03l1.523.63c.956.4 1.409 1.5 1.009 2.455-.397.957-1.497 1.41-2.454 1.012H7.54z"></path></svg>'
+    }, {
+        label: "",
+        url: "https://matsoor.space/",
+        iconSvg: '<svg role="img" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20Zm0 2a8 8 0 0 1 7.45 5H15.9a14.7 14.7 0 0 0-1.7-3.9A7.96 7.96 0 0 0 12 4Zm-2.2.9A12.6 12.6 0 0 1 10.1 7H4.55A8 8 0 0 1 9.8 4.9ZM4.06 9h6.03a16 16 0 0 0-.09 3H3.35a8.05 8.05 0 0 1 .71-3Zm.49 5h5.53c.2 1.5.56 2.7 1.02 3.6A8.02 8.02 0 0 1 4.55 14Zm6.05 0h3.28a16 16 0 0 1-1.04 3.63A13.8 13.8 0 0 1 10.6 14Zm4.8 0h4a8 8 0 0 1-6.25 5.9c.47-.9.9-2.1 1.1-3.6Zm4.49-2h-5.97a16 16 0 0 0-.09-3h6.03c.28.95.43 1.96.43 3ZM13.4 7h3.28a13.8 13.8 0 0 0-1.04-3.63A16 16 0 0 1 13.4 7Z"/></svg>'
+    }, ],
+
+    // Karty
+    playlist: {
+        youtubePlaylistId: "PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj"
+    },
+
+    pcSetup: {
+        images: ["assets/setup/1.jpg", "assets/setup/2.jpg", "assets/setup/3.jpg", "assets/setup/4.jpg", "assets/setup/5.jpg", "assets/setup/6.jpg", "assets/setup/7.jpg", "assets/setup/8.jpg", "assets/setup/9.jpg", ]
+    },
+
+    lego: {
+        images: ["assets/lego/preview1.jpg", "assets/lego/preview2.jpg", "assets/lego/preview3.jpg", "assets/lego/preview4.jpg", ],
+        currency: "PLN",
+        sets: [{
+            number: "42171",
+            name: "Mercedes-AMG F1 W14 E Performance",
+            price: 682.44,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/63/6376090/LEGO-Technic-42171-Mercedes-AMG-F1-W14-E-Performance-klocki.jpg"
+        }, {
+            number: "42083",
+            name: "Bugatti Chiron",
+            price: 1700.00,
+            img: "https://zklockow.pl/img/960/lego-technic-42083-bugatti-chiron-0.jpg"
+        }, {
+            number: "42172",
+            name: "McLaren P1",
+            price: 1422.99,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/67/6796074/LEGO-42172-Technic-McLaren-P1-zdjecie-glowne.jpg"
+        }, {
+            number: "42115",
+            name: "Lamborghini Sián FKP 37",
+            price: 1395.68,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/24/2494950/LEGO-Technic-Lamborghini-Sian-FKP-37-42115-1-1.jpg"
+        }, {
+            number: "42170",
+            name: "Kawasaki Ninja H2R",
+            price: 248.00,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/63/6376078/LEGO-42170-Technic-Motocykl-Kawasaki-Ninja-H2R-zawartosc.jpg"
+        }, {
+            number: "76217",
+            name: "Ja jestem Groot",
+            price: 199.00,
+            img: "https://ecsmedia.pl/cdn-cgi/image/format=webp,width=544,height=544,/c/lego-marvel-klocki-avengers-ja-jestem-groot-76217-b-iext197514488.jpg"
+        }, {
+            number: "10281",
+            name: "Drzewko bonsai",
+            price: 154.99,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/26/2663083/LEGO-Creator-Drzewko-bonsai-10281-front3.jpg"
+        }, {
+            number: "42173",
+            name: "Szary hipersamochód Koenigsegg Jesko Absolut",
+            price: 164.97,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/67/6741590/LEGO-42173-Technic-Szary-hipersamochod-Koenigsegg-Jesko-Absolut-zawartosc-x.jpg"
+        }, {
+            number: "0",
+            name: "Mercedes AMG GT",
+            price: 133.86,
+            img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQtjK8WROVBcK3zlK60vX6qU29D50vDzXWH7N3EwsOYLZ2p-i8CWKXXL89VCApQlb6Q2xP3Fys8kk-n79-3T6ME2thCFG9y3M1bu-xLvbRv6fr2QGFZn9c1YhbZ-nfH3VlxF7pFhC4&usqp=CAc"
+        }, {
+            number: "42138",
+            name: "Ford Mustang Shelby",
+            price: 278.99,
+            img: "https://image.ceneostatic.pl/data/products/120596963/f-lego-technic-42138-ford-mustang-shelby-gt500.jpg"
+        }, {
+            number: "10295",
+            name: "Porsche 911",
+            price: 548.79,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/29/2915118/LEGO-Creator-Porsche-911-10295-opakowanie1.jpg"
+        }, {
+            number: "42161",
+            name: "Lamborghini Huracán Tecnica",
+            price: 164.00,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/56/5696548/lego-technic-lamborghini-huracan-tecnica-42161-skos.jpg"
+        }, {
+            number: "42123",
+            name: "McLaren Senna GTR",
+            price: 179.00,
+            img: "https://image.ceneostatic.pl/data/products/101087243/i-lego-technic-42123-mclaren-senna-gtr.jpg"
+        }, {
+            number: "42151",
+            name: "Bolid Bugatti",
+            price: 154.00,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/43/4390596/LEGO-Technic-Bolid-Bugatti-42151-zawartrosc.jpg"
+        }, {
+            number: "42096",
+            name: "Porsche 911 RSR",
+            price: 897.00,
+            img: "https://www.lego.com/cdn/cs/set/assets/bltbad5cceab306b542/42096_Prod.png?format=webply&fit=bounds&quality=75&width=800&height=800&dpr=1"
+        }, {
+            number: "42111",
+            name: "Dom's Dodge Charger",
+            price: 559.00,
+            img: "https://zklockow.pl/img/lego-technic-42111-doms-dodge-charger-0.jpg"
+        }, {
+            number: "42125",
+            name: "Ferrari 488 GTE AF Corse 51",
+            price: 946.99,
+            img: "https://ecsmedia.pl/cdn-cgi/image/format=webp,width=544,height=544,/c/lego-technic-klocki-ferrari-488-gte-af-corse-51-42125-b-iext192762495.jpg"
+        }, {
+            number: "42154",
+            name: "Ford GT",
+            price: 391.99,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/52/5218174/LEGO-Technic-Ford-GT-wersja-z-2022-roku-42154-zawartosc.jpg"
+        }, {
+            number: "42177",
+            name: "Mercedes-Benz G 500 PROFESSIONAL Line 4x4",
+            price: 789.00,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/67/6741606/LEGO-42177-Technic-Mercedes-G500-4x4-zawartosc-x.jpg"
+        }, {
+            number: "42156",
+            name: "Peugeot 9X8 24H Le Mans Hybrid Hypercar",
+            price: 625.00,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/54/5407148/LEGO-Technic-Peugeot-9X8-24H-Le-Mans-Hybrid-Hypercar-42156-elementy7-1.jpg"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, {
+            number: "0",
+            name: "0",
+            price: 0.00,
+            img: "0"
+        }, ]
+    },
+
+    pcSpecs: {
+        currency: "PLN",
+        parts: [{
+            name: "CASE",
+            model: "Phanteks NV5 MK2 Black",
+            price: 459,
+            img: "https://cdn.x-kom.pl/i/setup/images/prod/big/product-small,,2024/7/pr_2024_7_30_13_40_43_579_00.jpg"
+        }, {
+            name: "MOTHERBOARD",
+            model: "Płyta główna MSI B850 Gaming Plus WiFi",
+            price: 926.47,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery_100_100/thumbnails/images/72/7294692/Plyta-glowna-MSI-B850-Gaming-Plus-WiFi-1.jpg"
+        }, {
+            name: "CPU",
+            model: "Procesor AMD Ryzen 7 9800X3D",
+            price: 2399,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery_100_100/thumbnails/images/71/7122428/Procesor-AMD-Ryzen-7-9800X3D.jpg"
+        }, {
+            name: "RAM",
+            model: "Pamięć RAM LEXAR Ares RGB 64GB (4x16GB) 6000MHz",
+            price: 1098,
+            img: "https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2023/8/pr_2023_8_3_13_7_54_278_01.jpg"
+        }, {
+            name: "SSD",
+            model: "Dysk LEXAR NM710 500GB SSD",
+            price: 186.74,
+            img: "https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2023/2/pr_2023_2_8_13_53_52_561_06.jpg"
+        }, {
+            name: "SSD",
+            model: "Dysk LEXAR NM620 1TB SSD",
+            price: 249,
+            img: "https://cdn.x-kom.pl/i/setup/images/prod/big/product-big,,2022/7/pr_2022_7_11_7_55_50_654_02.jpg"
+        }, {
+            name: "SSD",
+            model: "Dysk LEXAR NM790 2TB SSD",
+            price: 539,
+            img: "https://cdn.x-kom.pl/i/setup/images/prod/big/product-big,,2023/5/pr_2023_5_24_11_14_29_662_00.jpg"
+        }, {
+            name: "COOLER",
+            model: "DEEPCOOL Mystique 360 ARGB",
+            price: 702,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery_100_100/thumbnails/images/66/6684226/Chlodzenie-wodne-DEEPCOOL-Mystique-360-ARGB-1.jpg"
+        }, {
+            name: "PSU",
+            model: "CORSAIR RM1000x 1000W 80 Plus Gold ATX 3.1",
+            price: 775,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery_100_100/thumbnails/images/69/6989874/Zasilacz-CORSAIR-RM1000x-CP-9020271-EU-1.jpg"
+        }, {
+            name: "FANS x8",
+            model: "Prism 4Pro ARGB",
+            price: 143,
+            img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRK1JUNJ0gCeaYHfZVR_WjI83jA1yPu0bJ1E5nxGEwBd1m3CG-salDQ0gVV9K0QGcox74qvYM_5Ka8SbhR59sjfNYR4rXiTZW6v7eLQiVdjOmxUrgTSd7F3K-n2Sa-Orlbg5VN4RThXaGQ&usqp=CAc"
+        }, {
+            name: "CABLE",
+            model: "Lian Li Strimer 24-Pin RGB",
+            price: 314,
+            img: "https://images.morele.net/i1064/14200366_0_i1064.jpg"
+        }, {
+            name: "CABLE",
+            model: "Lian Li Strimer GPU 2x6+2-Pin RGB",
+            price: 220,
+            img: "https://images.morele.net/i1064/14473190_0_i1064.jpg"
+        }, {
+            name: "GPU",
+            model: "Karta graficzna XFX Swift Radeon RX 9070 XT Triple Fan Gaming Edition 16GB",
+            price: 3449,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery_100_100/thumbnails/images/75/7557266/Karta-graficzna-XFX-Swift-Radeon-RX-9070-XT-Triple-Fan-Gaming-Edition-1.jpg"
+        },
+        {
+            name: "EXTERNAL DRIVE",
+            model: "SAMSUNG Portable T7 500GB SSD",
+            price: 329,
+            img: "https://prod-api.mediaexpert.pl/api/images/gallery_100_100/thumbnails/images/23/2356058/Dysk-SAMSUNG-Portable-T7-500GB-SSD-Szary-front.jpg"
+        }, {
+            name: "HARDWARE MONITOR",
+            model: "Hardware Temperature Monitor",
+            price: 50,
+            img: "https://m.media-amazon.com/images/I/71rcZ3SbXiL.jpg"
+        }, ]
+    },
+
+    background: {
+        videoUrl: "assets/intro.mp4",
+        // 🔹 Twój plik
+        videoHasAudio: true // 🔹 Odtwarza audio z niego
+    },
+
+};
